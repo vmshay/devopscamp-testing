b73a2963b85cd08f72468b8ef780a2a76b9109fb
Vildan Shaimagsumov <you@example.com>
2023-08-29 16:05:37 +0700

    Add app & playbook

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/hostname', methods=['GET'])
+def get_hostname():
+    return os.uname().nodename
+
+@app.route('/author', methods=['GET'])
+def get_author():
+    return os.environ.get('AUTHOR', 'Unknown')
+
+@app.route('/id', methods=['GET'])
+def get_id():
+    return os.environ.get('UID', 'Unknown')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
