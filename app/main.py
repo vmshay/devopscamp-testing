7ed487997e8914c40546fcd7ca00334ab3b37112
Vildan Shaymagsumov <37113778+vmshay@users.noreply.github.com>
2023-09-04 01:27:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,5 +16,9 @@ def get_author():
 def get_id():
     return os.environ.get('UID', 'Unknown')
 
+@app.errorhandler(404)
+def pageNotFount(error):
+    return "Not found"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
